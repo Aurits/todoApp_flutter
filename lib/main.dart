b9dba54d37cd47b44d967eebe901478c6a8b0a46
Ambrose Alanda <alandaambrose@gmail.com>
2023-10-25 10:22:08 +0300

    Updates

--- lib/main.dart
+++ lib/main.dart
@@ -12,6 +12,7 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return const MaterialApp(
+      debugShowCheckedModeBanner: false,
       title: 'My Todo App',
       home: ToDoPage(title: 'My Todo'),
     );
--- lib/pages/todopage.dart
+++ lib/pages/todopage.dart
@@ -8,11 +8,43 @@ class ToDoPage extends StatefulWidget {
 }
 
 class _ToDoPageState extends State<ToDoPage> {
+  //text editing controller to access what the user typed
+
+  TextEditingController myController = TextEditingController();
+
+  void greetUser() {
+    print(myController.text);
+
+    setState(() {
+      greetingMessage = "Hello, " + myController.text;
+    });
+  }
+
+  String greetingMessage = "";
+
   @override
   Widget build(BuildContext context) {
-    return const Scaffold(
+    return Scaffold(
       body: Center(
-        child: TextField(),
+        child: Padding(
+          padding: const EdgeInsets.all(8.0),
+          child: Column(mainAxisAlignment: MainAxisAlignment.center, children: [
+            Text(greetingMessage),
+            TextField(
+              controller: myController,
+              decoration: const InputDecoration(
+                border: OutlineInputBorder(),
+                hintText: "Type your name",
+              ),
+            ),
+
+            //button
+            ElevatedButton(
+              onPressed: greetUser,
+              child: const Text("Tap"),
+            ),
+          ]),
+        ),
       ),
     );
   }
